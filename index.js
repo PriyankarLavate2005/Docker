04b90718545ad1b8dca673e93802d374cdff89b2
PriyankarLavate2005 <priyankalavate909@gmail.com>
2024-11-08 17:27:17 +0530

    Docker File Added

---
+++ index.js
@@ -0,0 +1,11 @@
+const express=require("express")
+const app=express()
+app.get('/',(req,res)=>{
+    res.json({message: 'OK'});
+})
+app.get('/home',(req,res)=>{
+    res.send("Hello ")
+})
+app.listen(3000,()=>{
+    console.log("server Started Sucessfully")
+})
